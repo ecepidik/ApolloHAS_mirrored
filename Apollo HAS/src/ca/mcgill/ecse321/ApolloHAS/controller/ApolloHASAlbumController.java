a091deae2be8116be6c5ef0d634cf07c5acd0575
ecepidik <ece.pidik@mail.mcgill.ca>
2016-02-21 19:43:52 -0500

    HAHAHAHAHAHAHA :|

--- Apollo HAS/src/ca/mcgill/ecse321/ApolloHAS/controller/ApolloHASAlbumController.java
+++ Apollo HAS/src/ca/mcgill/ecse321/ApolloHAS/controller/ApolloHASAlbumController.java
@@ -9,86 +9,108 @@ import ca.mcgill.ecse321.ApolloHAS.persistence.PersistenceXStream;
 
 public class ApolloHASAlbumController {
 	
-	Album album;
-	String name;
-	String artistName;
-	String releaseDate;
-	String inputNumberofSongs;
-	int numberOfSongs;
-
-	public ApolloHASAlbumController(String albName,String artName,String RlsDate,String numbOfSongs){
-		this.name = albName;
-		this.artistName = artName;
-
-		this.releaseDate = RlsDate;
-
-		this.inputNumberofSongs = numbOfSongs;
-
+	public ApolloHASAlbumController(){
 	}
-
-	public boolean checkVoids() {
-		if(name.length()==0 || name.trim().length() == 0){
-			return false;
-		}
-		else if(artistName.length()==0 || artistName.trim().length() == 0){
-			return false;
-		}
-		else if(releaseDate.length()==0 || releaseDate.trim().length() == 0){
-			return false;
-			}
-		else if(inputNumberofSongs.length()==0 || inputNumberofSongs.trim().length() == 0){
-			return false;
-			}
-		return true;
-		}
-
-	public boolean checkFormat() {
-		int i =0;
-		try {
-			i  = Integer.parseInt(inputNumberofSongs);
-			
-		} catch (NumberFormatException e) {
-			return false;
-		}		
+	
+	public Artist createArtist(String name) {
+		Artist artist = new Artist(name);
+		HAS manager = HAS.getInstance();
+		manager.addArtist(artist);
+		PersistenceXStream.saveToXMLwithXStream(manager);
 		
-		numberOfSongs = i;
-		
-		return true;
+		return artist;
 	}
-	public boolean checkDate() {
-
-		Artist artist = new Artist(artistName);
-		java.sql.Date parsed;
-		try {
-			parsed = java.sql.Date.valueOf(releaseDate);
-			java.sql.Date sql = new java.sql.Date(parsed.getTime());
-			album = new Album(name,sql,artist);
-			artist.addAlbum(album);
+	
+	public Album createAlbum(String name, Date date, Artist artist) {
+		Album album = new Album(name, date, artist);
+		HAS manager = HAS.getInstance();
+		manager.addAlbum(album);
+		PersistenceXStream.saveToXMLwithXStream(manager);
 		
-		} catch (IllegalArgumentException e) {
-			return false;
-		}
-			
-		artist.addAlbum(album);
-		saveArtist(artist);
-		saveAlbum();
-		return true;
-	}
-
-
-	private void saveArtist(Artist artist) {
-		HAS hs = HAS.getInstance();
-			
-		hs.addArtist(artist);
-		PersistenceXStream.saveToXMLwithXStream(hs);
-		}
-		
-	private void saveAlbum() {
-		HAS hs = HAS.getInstance();
-		
-		hs.addAlbum(album);
-
-		PersistenceXStream.saveToXMLwithXStream(hs);
-		}
-
-	}
+		return album;
+	}	
+}
+	
+//	Album album;
+//	String name;
+//	String artistName;
+//	String releaseDate;
+//	String inputNumberofSongs;
+//	int numberOfSongs;
+//
+//	public ApolloHASAlbumController(String albName,String artName,String RlsDate,String numbOfSongs){
+//		this.name = albName;
+//		this.artistName = artName;
+//
+//		this.releaseDate = RlsDate;
+//
+//		this.inputNumberofSongs = numbOfSongs;
+//
+//	}
+//
+//	public boolean checkVoids() {
+//		if(name.length()==0 || name.trim().length() == 0){
+//			return false;
+//		}
+//		else if(artistName.length()==0 || artistName.trim().length() == 0){
+//			return false;
+//		}
+//		else if(releaseDate.length()==0 || releaseDate.trim().length() == 0){
+//			return false;
+//			}
+//		else if(inputNumberofSongs.length()==0 || inputNumberofSongs.trim().length() == 0){
+//			return false;
+//			}
+//		return true;
+//		}
+//
+//	public boolean checkFormat() {
+//		int i =0;
+//		try {
+//			i  = Integer.parseInt(inputNumberofSongs);
+//			
+//		} catch (NumberFormatException e) {
+//			return false;
+//		}		
+//		
+//		numberOfSongs = i;
+//		
+//		return true;
+//	}
+//	public boolean checkDate() {
+//
+//		Artist artist = new Artist(artistName);
+//		java.sql.Date parsed;
+//		try {
+//			parsed = java.sql.Date.valueOf(releaseDate);
+//			java.sql.Date sql = new java.sql.Date(parsed.getTime());
+//			album = new Album(name,sql,artist);
+//			artist.addAlbum(album);
+//		
+//		} catch (IllegalArgumentException e) {
+//			return false;
+//		}
+//			
+//		artist.addAlbum(album);
+//		saveArtist(artist);
+//		saveAlbum();
+//		return true;
+//	}
+//
+//
+//	private void saveArtist(Artist artist) {
+//		HAS hs = HAS.getInstance();
+//			
+//		hs.addArtist(artist);
+//		PersistenceXStream.saveToXMLwithXStream(hs);
+//		}
+//		
+//	private void saveAlbum() {
+//		HAS hs = HAS.getInstance();
+//		
+//		hs.addAlbum(album);
+//
+//		PersistenceXStream.saveToXMLwithXStream(hs);
+//		}
+//
+//	}
--- mobile-workspace/app/src/main/java/ca/mcgill/ecse321/apollohas/AddAlbumActivity.java
+++ mobile-workspace/app/src/main/java/ca/mcgill/ecse321/apollohas/AddAlbumActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -23,7 +25,7 @@ import ca.mcgill.ecse321.ApolloHAS.controller.*;
 
 public class AddAlbumActivity extends AppCompatActivity {
 
-    private ArrayList<Album> albums;
+    private HashMap<Integer, Album> albums;
     private HashMap<Integer, Song> songs;
     private HashMap<Integer, Artist> artists;
 
@@ -31,50 +33,86 @@ public class AddAlbumActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_album);
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//        refreshData();
     }
 
-//    private void refreshData() {
-//        //Initialize the data in the participant spinner
-//        HAS has = HAS.getInstance();
-//        Spinner spinnerAlbum = (Spinner) findViewById(R.id.albumspinner);
-//        ArrayAdapter<CharSequence> albumAdapter = new
-//                ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item);
-//        albumAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        this.albums = new HashMap<Integer, Album>();
-//
-//        int i = 0;
-//
-//        for (Iterator<Album> albums = has.getAlbum().iterator(); albums.hasNext(); i++) {
-//            Album album = albums.next();
-//            albumAdapter.add(album.getName());
-//            this.albums.put(i, album);
-//            //this.participants.put(p.getName(), p);
-//        }
-//        spinnerAlbum.setAdapter(albumAdapter);
-//    }
 
+    private void refreshData() {
+        HAS manager = HAS.getInstance();
+        Spinner spinnerAlbum = (Spinner) findViewById(R.id.albumspinner);
+        ArrayAdapter<CharSequence> albumAdapter = new
+                ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item);
+        albumAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        this.albums = new HashMap<Integer, Album>();
+        int i = 0;
+        for (Iterator<Album> albums = manager.getAlbum().iterator(); albums.hasNext(); i++) {
+            Album album = albums.next();
+            albumAdapter.add(album.getName());
+            this.albums.put(i, album);
+        }
+        spinnerAlbum.setAdapter(albumAdapter);
+
+        this.artists = new HashMap<Integer, Artist>();
+        int k = 0;
+        for (Iterator<Artist> artists = manager.getArtist().iterator(); artists.hasNext(); i++) {
+            Artist artist = artists.next();
+//            artistAdapter.add(artist.getName());
+            this.artists.put(k, artist);
+        }
+        //spinnerAlbum.setAdapter(albumAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     public void addAlbum(View v) {
+        ApolloHASAlbumController hasc = new ApolloHASAlbumController();
+
         TextView tvAlbumName = (TextView) findViewById(R.id.album_name);
         String albumName = tvAlbumName.getText().toString();
-        TextView tvArtistName = (TextView) findViewById(R.id.artist_name);
 
-        TextView tvSongNum = (TextView) findViewById(R.id.number_songs);
+        TextView tvArtistName = (TextView) findViewById(R.id.artist_name);
+        String artistName = tvArtistName.toString();
+        Artist artist = hasc.createArtist(artistName);
 
         TextView tvReleaseDate = (TextView) findViewById(R.id.release_date);
         Date releaseDate = unbundleDateBundle(getDateFromLabel(tvReleaseDate.getText()));
 
-        ApolloHASAlbumController newAlbum = new ApolloHASAlbumController(albumName,tvReleaseDate.toString(), tvArtistName.toString(),tvSongNum.toString());
-
-        //albums.add(newAlbum);
-
+        hasc.createAlbum(albumName, releaseDate, artist);
+        refreshData();
     }
-
-    public void addSong2Album(View v) {
-
-    }
-
-
 
     public void showDatePickerDialog(View v) {
         TextView tf = (TextView) v;
