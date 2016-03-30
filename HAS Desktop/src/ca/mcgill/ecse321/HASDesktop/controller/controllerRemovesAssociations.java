2818623080b3758251d6e27bbb0237677d568919
ecepidik <ece.pidik@mail.mcgill.ca>
2016-03-30 14:10:13 -0400

    controllerRemovesAssociations

---
+++ HAS Desktop/src/ca/mcgill/ecse321/HASDesktop/controller/controllerRemovesAssociations.java
@@ -0,0 +1,22 @@
+package ca.mcgill.ecse321.HASDesktop.controller;
+
+import ca.mcgill.ecse321.HASDesktop.model.Album;
+import ca.mcgill.ecse321.HASDesktop.model.Genre;
+import ca.mcgill.ecse321.HASDesktop.model.Playlist;
+import ca.mcgill.ecse321.HASDesktop.model.Song;
+
+public class controllerRemovesAssociations {
+	
+	public void removeSongsFromAlbum(Song aSong, Album aAlbum) {
+		
+	}
+	
+	public void removeSongsFromPlaylist(Song aSong, Playlist aPlaylist) {
+		
+	}
+	
+	public void removeGenreFromSong(Genre aGenre, Song aSong) {
+		
+	}
+	
+}
