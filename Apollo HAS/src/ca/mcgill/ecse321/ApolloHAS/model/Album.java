00a5f5b1ac58af668ed05150435559895492d139
courtiswright <courtney.wright@mail.mcgill.ca>
2016-03-30 13:00:46 -0400

    Update Model to Include Genre

---
+++ Apollo HAS/src/ca/mcgill/ecse321/ApolloHAS/model/Album.java
@@ -0,0 +1,206 @@
+package ca.mcgill.ecse321.ApolloHAS.model;
+
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.22.0.5146 modeling language!*/
+
+
+import java.sql.Date;
+import java.util.*;
+
+// line 23 "HAS_model.ump"
+public class Album
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //Album Attributes
+  private String name;
+  private Date releaseDate;
+
+  //Album Associations
+  private List<Song> songs;
+  private Artist artist;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Album(String aName, Date aReleaseDate, Artist aArtist)
+  {
+    name = aName;
+    releaseDate = aReleaseDate;
+    songs = new ArrayList<Song>();
+    boolean didAddArtist = setArtist(aArtist);
+    if (!didAddArtist)
+    {
+      throw new RuntimeException("Unable to create album due to artist");
+    }
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setName(String aName)
+  {
+    boolean wasSet = false;
+    name = aName;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setReleaseDate(Date aReleaseDate)
+  {
+    boolean wasSet = false;
+    releaseDate = aReleaseDate;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public Date getReleaseDate()
+  {
+    return releaseDate;
+  }
+
+  public Song getSong(int index)
+  {
+    Song aSong = songs.get(index);
+    return aSong;
+  }
+
+  public List<Song> getSongs()
+  {
+    List<Song> newSongs = Collections.unmodifiableList(songs);
+    return newSongs;
+  }
+
+  public int numberOfSongs()
+  {
+    int number = songs.size();
+    return number;
+  }
+
+  public boolean hasSongs()
+  {
+    boolean has = songs.size() > 0;
+    return has;
+  }
+
+  public int indexOfSong(Song aSong)
+  {
+    int index = songs.indexOf(aSong);
+    return index;
+  }
+
+  public Artist getArtist()
+  {
+    return artist;
+  }
+
+  public static int minimumNumberOfSongs()
+  {
+    return 0;
+  }
+
+  public boolean addSong(Song aSong)
+  {
+    boolean wasAdded = false;
+    if (songs.contains(aSong)) { return false; }
+    songs.add(aSong);
+    wasAdded = true;
+    return wasAdded;
+  }
+
+  public boolean removeSong(Song aSong)
+  {
+    boolean wasRemoved = false;
+    if (songs.contains(aSong))
+    {
+      songs.remove(aSong);
+      wasRemoved = true;
+    }
+    return wasRemoved;
+  }
+
+  public boolean addSongAt(Song aSong, int index)
+  {  
+    boolean wasAdded = false;
+    if(addSong(aSong))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfSongs()) { index = numberOfSongs() - 1; }
+      songs.remove(aSong);
+      songs.add(index, aSong);
+      wasAdded = true;
+    }
+    return wasAdded;
+  }
+
+  public boolean addOrMoveSongAt(Song aSong, int index)
+  {
+    boolean wasAdded = false;
+    if(songs.contains(aSong))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfSongs()) { index = numberOfSongs() - 1; }
+      songs.remove(aSong);
+      songs.add(index, aSong);
+      wasAdded = true;
+    } 
+    else 
+    {
+      wasAdded = addSongAt(aSong, index);
+    }
+    return wasAdded;
+  }
+
+  public boolean setArtist(Artist aArtist)
+  {
+    boolean wasSet = false;
+    if (aArtist == null)
+    {
+      return wasSet;
+    }
+
+    Artist existingArtist = artist;
+    artist = aArtist;
+    if (existingArtist != null && !existingArtist.equals(aArtist))
+    {
+      existingArtist.removeAlbum(this);
+    }
+    artist.addAlbum(this);
+    wasSet = true;
+    return wasSet;
+  }
+
+  public void delete()
+  {
+    songs.clear();
+    Artist placeholderArtist = artist;
+    this.artist = null;
+    placeholderArtist.removeAlbum(this);
+  }
+
+  // line 28 "HAS_model.ump"
+   public void updateInfo(){
+    
+  }
+
+
+  public String toString()
+  {
+	  String outputString = "";
+    return super.toString() + "["+
+            "name" + ":" + getName()+ "]" + System.getProperties().getProperty("line.separator") +
+            "  " + "releaseDate" + "=" + (getReleaseDate() != null ? !getReleaseDate().equals(this)  ? getReleaseDate().toString().replaceAll("  ","    ") : "this" : "null") + System.getProperties().getProperty("line.separator") +
+            "  " + "artist = "+(getArtist()!=null?Integer.toHexString(System.identityHashCode(getArtist())):"null")
+     + outputString;
+  }
+}
---
+++ Apollo HAS/src/ca/mcgill/ecse321/ApolloHAS/model/Room.java
@@ -0,0 +1,93 @@
+package ca.mcgill.ecse321.ApolloHAS.model;
+
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.22.0.5146 modeling language!*/
+
+
+
+// line 38 "HAS_model.ump"
+public class Room
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //Room Attributes
+  private String name;
+  private int volume;
+  private boolean mute;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Room(String aName, int aVolume, boolean aMute)
+  {
+    name = aName;
+    volume = aVolume;
+    mute = aMute;
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setName(String aName)
+  {
+    boolean wasSet = false;
+    name = aName;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setVolume(int aVolume)
+  {
+    boolean wasSet = false;
+    volume = aVolume;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setMute(boolean aMute)
+  {
+    boolean wasSet = false;
+    mute = aMute;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public int getVolume()
+  {
+    return volume;
+  }
+
+  public boolean getMute()
+  {
+    return mute;
+  }
+
+  public void delete()
+  {}
+
+  // line 43 "HAS_model.ump"
+   public void updateInfo(){
+    
+  }
+
+
+  public String toString()
+  {
+	  String outputString = "";
+    return super.toString() + "["+
+            "name" + ":" + getName()+ "," +
+            "volume" + ":" + getVolume()+ "," +
+            "mute" + ":" + getMute()+ "]"
+     + outputString;
+  }
+}
