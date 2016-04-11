9f4ca5fe6581a636332b6e385a0e5c79d15f7f58
courtiswright <courtney.wright@mail.mcgill.ca>
2016-04-11 19:59:38 -0400

    Debug Add Songs To Playlist

--- mobile-workspace/app/src/main/java/ca/mcgill/ecse321/apollohas/AddSongsToPlaylist.java
+++ mobile-workspace/app/src/main/java/ca/mcgill/ecse321/apollohas/AddSongsToPlaylist.java
@@ -5,23 +5,57 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import ca.mcgill.ecse321.HASDesktop.model.HAS;
+import ca.mcgill.ecse321.HASDesktop.model.Song;
+import ca.mcgill.ecse321.HASDesktop.persistence.PersistenceHAS;
 
 public class AddSongsToPlaylist extends AppCompatActivity {
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> list = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_songs_to_playlist);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        PersistenceHAS phas = new PersistenceHAS();
+        phas.loadApolloHASModel();
+        HAS hs = HAS.getInstance();
+
+        ArrayList<String> songTitles = new ArrayList<String>();
+
+        int i = 0;
+        for (Iterator<Song> songs = hs.getSongs().iterator(); songs.hasNext(); i++) {
+            Song song = songs.next();
+            songTitles.add(song.getName());
+
+        }
+
+        ArrayAdapter<String> songAdapter =new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, songTitles);
+
+        ListView listView = (ListView) findViewById(R.id.listView3);
+        listView.setAdapter(songAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                
             }
         });
     }
--- mobile-workspace/app/src/main/java/ca/mcgill/ecse321/apollohas/CreatePlaylistActivity.java
+++ mobile-workspace/app/src/main/java/ca/mcgill/ecse321/apollohas/CreatePlaylistActivity.java
@@ -1,11 +1,13 @@
 package ca.mcgill.ecse321.apollohas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class CreatePlaylistActivity extends AppCompatActivity {
 
@@ -16,7 +18,11 @@ public class CreatePlaylistActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+    }
 
+    public void goAddSongsToPlaylistPage(View v) {
+        Button button = (Button) v;
+        startActivity(new Intent(getApplicationContext(), AddSongsToPlaylist.class));
     }
 
 }
